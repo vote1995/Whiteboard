9d9bf411b63d2583ed79aaf8e94c2ca869290871
worakan5071 <122747643+worakan5071@users.noreply.github.com>
2023-05-14 22:55:49 +0700

    socket

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.0.0/fabric.min.js"></script>
+const express = require('express');
+const socketio = require('socket.io');
+const app = express();
 
-var canvas = new fabric.Canvas('canvas');
+app.set("view engine",'ejs');
+app.use(express.static('css'));
+app.use("/css", express.static(__dirname + "css"));
+app.use(express.static('js'));
+app.use("/js",express.static(__dirname + "js"));
+app.use(express.static('views'));
+app.use("/views",express.static(__dirname + "views"));
 
-// create a rectangle object
-var rect = new fabric.Rect({
- left: 100,
- top: 100,
- fill: 'red',
- width: 20,
- height: 20
+
+
+app.get("/",(req,res) =>{
+    res.render('main.ejs');
 });
 
-// "add" rectangle onto canvas
-canvas.add(
-    new fabric.Rect({ top: 100, left: 100, width: 50, height: 50, fill: '#f55' }),
-    new fabric.Circle({ top: 140, left: 230, radius: 75, fill: 'green' }),
-    new fabric.Triangle({ top: 300, left: 210, width: 100, height: 100, fill: 'blue' })
-    );
-   
-   fabric.Image.fromURL('https://upload.wikimedia.org/wikipedia/commons/d/d7/Sad-pug.jpg', function(img) {
-    img.set({ left: 400, top: 350, angle: 30});
-    img.scaleToHeight(100);
-    img.scaleToWidth(200);
-    canvas.add(img);
-    });
-   
-   canvas.isDrawingMode = true; //For free hand drawing
-
-canvas.on('object:added', function(options) {
-    /* sync object add */
+const sever = app.listen(process.env.PORT || 3000, () =>{
+    console.log('sever is runing....');
 });
-canvas.on('object:removed', function(options) {
-   /* sync object removal*/
+
+//
+const io = socketio(sever);
+
+io.on("connection",socket=>{
+    console.log("New user connected");
+    
+    socket.username="Unknow"
+
+    socket.on("change_username",data =>{
+        socket.username = data.username
+    })
 });
